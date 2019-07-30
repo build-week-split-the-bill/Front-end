3700740bf698313ffa2b29e8b159f821c438d5ea
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-07-29 19:25:54 -0500

    Login prototype.

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -24,7 +24,7 @@ const Login = () => (
         </Segment>
       </Form>
       <Message>
-        New to us? <a href='#'>Sign Up</a>
+        New here? <a href='#'>Create a new account</a>
       </Message>
     </Grid.Column>
   </Grid>
