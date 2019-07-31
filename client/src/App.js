e04f6b8456ae561ac12e920525bdd9848b76c929
Shawn McManus <xsvfan76@gmail.com>
2019-07-31 14:49:56 -0600

    finished-register-and-success-alert

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login.js'
+import Register from './Components/Register.js';
+
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Register />
     </div>
   );
 }
